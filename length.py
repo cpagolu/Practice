01ad4f8241a802ae56f73fc1f3e805def76e9122
cpagolu <144611778+cpagolu@users.noreply.github.com>
2023-09-10 14:03:54 -0300

    Add files via upload

---
+++ length.py
@@ -0,0 +1,16 @@
+myfile = open("words.txt")
+
+def len(myfile):
+	for letter in myfile:
+		word=letter.strip()
+		#word=letter
+		while let in word:
+			global let
+			let =0
+			let=word.readline()
+			index = 0
+			if let[index]<=len(word[:4]):
+				print(i)
+				index = index + 1
+			return 0
+len(myfile)
